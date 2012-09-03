4324ea5791ff5eec0ce170034e3f19d45e265e94
Ricky Sheaves <r@calebscreek.com>
2012-09-03 13:01:41 -0400

    Add default 'rails new depot' output
    Did need to add 'therubyracer' to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'd4f64249f85003af3d5e65cbbe9537ad14be349f0662aacd9f8c72bf24f543d63c1d43c790df4fbf8361d79568fe51609ec2483ad96aa0d093d97c73d3bd3904'
